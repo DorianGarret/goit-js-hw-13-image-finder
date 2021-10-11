a26394ae7b693d7ce914bd24d5934431cc7f7234
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-10-11 13:06:45 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss'
+
+import ApiService from './js/api-sevice'
+
+const apiService = new ApiService()
+
+apiService.images = 'cat'
+
+apiService.fetchImages().then(hits => console.log(hits))
