231dc9991eccfab04988df68365c59e2fed4c43f
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-10-14 12:15:56 +0300

    first commit

--- src/js/api-sevice.js
+++ src/js/api-sevice.js
@@ -1,26 +1,11 @@
 const API_KEY = '23790821-6777a11e2db191613ff9bf1d2'
 const BASE_URL = 'https://pixabay.com/api/'
 
-export default class ApiService {
-    constructor() {
-        this.searchImages = ''
-        this.page = 1
-    }
-
-    fetchImages() {
-        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchImages}&page=${this.page}&per_page=15&key=${API_KEY}`
-        return fetch(url).then(respose => respose.json())
-    }
-
-    resetPage() {
-        this.page = 1
-    }
-
-    get images() {
-        return this.searchImages
-    }
-
-    set images(newImages) {
-        this.searchImages = newImages
-    }
+function fetchImage(searchImages) {
+    return fetch(
+        `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchImages}&page=1&per_page=12`,
+    )
+        .then(result => result.json())
+        .then(({ hits }) => hits)
 }
+export default { fetchImage }
--- src/js/render-card.js
+++ src/js/render-card.js
@@ -1,16 +1,28 @@
+import getRefs from './get-refs'
+import API from './api-sevice'
 import imageCardTpl from '../handlebars/image-card.hbs'
 
-const cardContainer = document.querySelector('.js-card-container')
+const refs = getRefs()
 
-fetch(
-    'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=horizont&page=1&per_page=12&key=23790821-6777a11e2db191613ff9bf1d2',
-)
-    .then(result => result.json())
-    .then(data => {
-        return data.hits
-    })
-    .then(cat => {
-        const markup = imageCardTpl(cat)
-        console.log(markup)
-        cardContainer.innerHTML = markup
-    })
+refs.searchForm.addEventListener('submit', onSearch)
+
+function onSearch(event) {
+    event.preventDefault()
+    refs.cardContainer.innerHTML = ''
+
+    let form = event.currentTarget
+    let searchQuery = form.elements.query.value
+
+    API.fetchImage(searchQuery)
+        .then(renderImageCard)
+        .catch(onFetchError)
+        .finally(() => form.reset())
+}
+
+function renderImageCard(image) {
+    refs.cardContainer.insertAdjacentHTML('beforeend', imageCardTpl(image))
+}
+
+function onFetchError() {
+    alert('ERROR')
+}
